06e1618b3df25c0a6b7f63138b9b030a627efa8d
zhaoyinglong <zhaoyinglong@okjiaoyu.cn>
2019-09-20 14:56:14 +0800

    改项目层级

---
+++ app/src/main/java/com/okay/multirecycleitem/adapter/OnePicDelegateAdapter.kt
@@ -0,0 +1,40 @@
+package com.okay.multirecycleitem.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.okay.lib.IDelegateAdapter
+import com.okay.multirecycleitem.R
+import com.okay.multirecycleitem.data.News
+
+/**
+ * Create by zyl
+ * @date 2019-09-19
+ */
+class OnePicDelegateAdapter : IDelegateAdapter<News> {
+
+    override fun isForViewType(t: News): Boolean {
+        return t.type == 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_text, parent, false)
+        return MyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, t: News?) {
+        holder as MyHolder
+        holder.textView?.text = t?.desc
+    }
+
+    inner class MyHolder : RecyclerView.ViewHolder {
+
+        var textView: TextView? = null
+
+        constructor(itemView: View) : super(itemView) {
+            textView = itemView.findViewById<TextView>(R.id.tv)
+        }
+    }
+}
